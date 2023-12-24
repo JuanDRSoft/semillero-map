cf392c77b4b1612d063cf777521cd4269562801b
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2023-12-24 11:55:29 -0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRX0-r3s_xryfx-ro6aZvwLYQlyvSdGFs",
+  authDomain: "semillero-react.firebaseapp.com",
+  projectId: "semillero-react",
+  storageBucket: "semillero-react.appspot.com",
+  messagingSenderId: "144495666351",
+  appId: "1:144495666351:web:60fc7f376f42d7d7c1a05a",
+  measurementId: "G-KD5CWC9PPV",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
+
+export { storage, firebase as default };
